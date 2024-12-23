86612852c1f165b61147c1d44d15dbad7e50f8fa
VeeKay <kariavedant2@gmail.com>
2024-12-23 13:03:00 +0530

    Initial Commit

---
+++ MouseTimer2/MouseTimer2App.swift
@@ -0,0 +1,17 @@
+//
+//  MouseTimer2App.swift
+//  MouseTimer2
+//
+//  Created by VEEKAY on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MouseTimer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MouseTimer2Tests/MouseTimer2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MouseTimer2Tests.swift
+//  MouseTimer2Tests
+//
+//  Created by VEEKAY on 23.12.2024.
+//
+
+import Testing
+@testable import MouseTimer2
+
+struct MouseTimer2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
